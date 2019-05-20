cac6e72c13cbabf2241dc2eb473205e07b8abe61
dbwjd1094 <dbwjd1094@ncu.ac.kr>
2019-05-20 23:41:29 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
+import axios from 'axios';
 
-//map을 통해서 만든 movie는 현 사이클의 현재 엘리먼트를 의미한다.
+//map을 통해서 만든 movie 현 사이클의 현재 엘리먼트를 의미한다.
 //index는 우리가 현재 제공하는 리스트의 숫자를 의미, 그래서 key prop으롤 index를 작성하도록 한다. index는 0부터 3
 class App extends Component {
+
   // componentWillMount(){
   //   console.log('will mount');
   // }
@@ -21,30 +23,39 @@ class App extends Component {
     //fn(function),0000초라고 작성하면 0000초 후엥 페이지가 로드하고 작업이 시작하는 것
     //function() :: ()=>
     //컴포넌트가 mount하면 페이지 로드 후 1 초 후에, 작업이 보여진다.
-    setTimeout(() => {
-      this.setState({
-        movies: [
-          {
-            title:"spiderman1",
-            poster:"http://esquirekorea.co.kr/wp-content/uploads/2018/11/esq-201811-132-002.jpg"
-          },
-          {
-            title:"avengers2",
-            poster:"http://ticketimage.interpark.com/Movie/still_image/V14/V1400753p_01.gif"
-          },
-          {
-            title:"avengers3",
-            poster:"http://img.movist.com/?img=/x00/05/01/14_p1.jpg"
-          },
-          {
-            title:"avengers4",
-            poster:"http://img.movist.com/?img=/x00/05/12/35_p1.jpg"
-          }
-        ]
-      })
-    },5000)
+    // setTimeout(() => {
+    //   this.setState({
+    //     movies: [
+    //       {
+    //         title:"spiderman1",
+    //         poster:"http://esquirekorea.co.kr/wp-content/uploads/2018/11/esq-201811-132-002.jpg"
+    //       },
+    //       {
+    //         title:"avengers2",
+    //         poster:"http://ticketimage.interpark.com/Movie/still_image/V14/V1400753p_01.gif"
+    //       },
+    //       {
+    //         title:"avengers3",
+    //         poster:"http://img.movist.com/?img=/x00/05/01/14_p1.jpg"
+    //       },
+    //       {
+    //         title:"avengers4",
+    //         poster:"http://img.movist.com/?img=/x00/05/12/35_p1.jpg"
+    //       }
+    //     ]
+    //  })
+    // },5000)
     //페이스북이나 인스타그램처럼 로딩 후에 뜨는 추가 컨텐츠를 밑으로 땡기고 로딩후에 뜨게 만드는 작업 같은 것들
+    //const req = new Request('https://yts.ag/api/v2/list_movies.json');
+    //,{
+    //   method : 'GET',
+    //   headers: {'Content-Type' : 'application/json',
+    // 'Access-Control-Allow-Origin':'*'},
+    //}
+    this._getMovies();
   }
+
+
   // movies: [
   //   //...this.state.movies, //이전 영화 리스트를 그대로 두고 그 후에 영화를 추가하라는 것
   //
@@ -58,10 +69,32 @@ class App extends Component {
   //함수이름 지을때 보통 _를 시작으로 지음
     //movies 라는 variable에 데이터를 저장했다.
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index}/>
+    const movies = this.state.movies.map(movie => {
+      return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id}/>
     })
     return movies;
+  }
+
+//이전 작업이 끝나는 것을 기다리지 않고 사용할 때
+   _getMovies = async () => {
+    //await 하는 것은 call api 기능이 끝나는 것을 기다리고
+    const movies = await this._callApi()
+    //이 컴포넌트의 set state를 movies로 하는 것은 call api의 return value 값이 된다.
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return axios.get('https://yts.ag/api/v2/list_movies.json?sort_by=rating',{
+      params:{
+        id:1
+      }
+    })
+    .then(response => response.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err));
+    //return fetch('https://yts.ag/api/v2/list_movies.json')
   }
 
   //데이터가 없을때 로딩을 띄우고, 있을때 영화정보가 보이도록
@@ -73,5 +106,10 @@ class App extends Component {
     );
   }
 }
+//
+// var express = require('express');
+// var cors = require('cors');
+// var app = express();
+// app.use(cors());
 
 export default App;
--- src/Movie.js
+++ src/Movie.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
@@ -6,35 +6,59 @@ import './Movie.css';
 //항상 return을 해야한다
 
 
-class Movie extends Component{
-    //이렇게하면 title과 poster 값은 string 값만 올수 있다.
-    //부모 컴포넌트가 string을 보내면, PropTypes로 string이라는 것을 체크할 수 있다.
-  static propTypes = {
-    title: PropTypes.string.isRequired, //isRequired은 이 값은 무조건 있어야 한다. 없으면 에러가뜸
-    poster: PropTypes.string.isRequired
-  }
+// class Movie extends Component{
+//     //이렇게하면 title과 poster 값은 string 값만 올수 있다.
+//     //부모 컴포넌트가 string을 보내면, PropTypes로 string이라는 것을 체크할 수 있다.
+//   static propTypes = {
+//     title: PropTypes.string.isRequired, //isRequired은 이 값은 무조건 있어야 한다. 없으면 에러가뜸
+//     poster: PropTypes.string.isRequired
+//   }
+//
+//   render(){
+//     return(
+//       <div>
+//         <MoviePoster poster={this.props.poster}/>
+//         <h1>{this.props.title}</h1>
+//       </div>
+//     );
+//   }
+// }
 
-  render(){
-    return(
-      <div>
-        <MoviePoster poster={this.props.poster}/>
-        <h1>{this.props.title}</h1>
-      </div>
-    );
-  }
+// class MoviePoster extends Component{
+//
+//   static propTypes={
+//     poster : PropTypes.string.isRequired
+//   }
+//
+//   render(){
+//     return(
+//       <img src={this.props.poster} alt="Movie Poster"/>
+//     );
+//   }
+// }
+
+function Movie({title, poster}){
+  return(
+    <div>
+      <MoviePoster poster={poster}/>
+      <h1>{title}</h1>
+    </div>
+  );
 }
 
-class MoviePoster extends Component{
+Movie.propTypes = {
+  title: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired
+}
 
-  static propTypes={
-    poster : PropTypes.string.isRequired
-  }
+function MoviePoster({poster}){
+  return(
+    <img src={poster} alt="Movie Poster"/>
+  );
+}
 
-  render(){
-    return(
-      <img src={this.props.poster} alt="Movie Poster"/>
-    );
-  }
+MoviePoster.propTypes = {
+  poster : PropTypes.string.isRequired
 }
 
 export default Movie;
